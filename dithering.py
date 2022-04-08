f3c7b8e0590f826732925055c2f449c729d5209e
José Aniceto <jose.pedro.aniceto@gmail.com>
2022-04-08 18:08:18 +0100

    Added CLI

--- dithering.py
+++ dithering.py
@@ -1,50 +1,14 @@
-from dataclasses import dataclass
 from typing import Tuple
-# import sys
-from os import mkdir
+from pathlib import Path
+
 from PIL import Image, ImageDraw
+import typer
+
+from Color import Color
+from palletes import PALLETE
 
 
-@dataclass
-class Color:
-    r: int
-    g: int
-    b: int
-
-
-# Pallete B&W
-PALLETE_BW = [
-    Color(0, 0, 0),
-    Color(255, 255, 255)
-]
-
-# Pallete 1
-PALLETE_1 = [
-    Color(190, 0, 57),
-    Color(255, 69, 0),
-    Color(255, 168, 0),
-    Color(255, 214, 53),
-    Color(0, 163, 104),
-    Color(0, 204, 120),
-    Color(126, 237, 86),
-    Color(0, 117, 111),
-    Color(0, 158, 170),
-    Color(36, 80, 164),
-    Color(54, 144, 234),
-    Color(81, 233, 244),
-    Color(73, 58, 193),
-    Color(106, 92, 255),
-    Color(129, 30, 159),
-    Color(180, 74, 192),
-    Color(255, 56, 129),
-    Color(255, 153, 170),
-    Color(109, 72, 47),
-    Color(212, 215, 217),
-    Color(156, 105, 38),
-    Color(0, 0, 0),
-    Color(137, 141, 144),
-    Color(255, 255, 255),
-]
+app = typer.Typer()
 
 
 def calc_distance(color1: Color, color2: Color):
@@ -52,7 +16,7 @@ def calc_distance(color1: Color, color2: Color):
     return ( (color1.r - color2.r)**2 + (color1.g - color2.g)**2 + (color1.b - color2.b)**2 )**0.5
 
 
-def get_nearest_color(old_color: Color, pallete=PALLETE_BW):
+def get_nearest_color(old_color: Color, pallete):
     """Find the nearest color to the original color present in the selected color pallete."""
     distance = 500  # max distance between colors in the RGB space is 441.6 (black and white)
     nearest_color = Color(0, 0, 0)
@@ -93,13 +57,13 @@ def multiply_color(color: Color, value: float):
     )
 
 
-def render(image: Image, output: str = 'imgout.png'):
+def render(image: Image, img_path: Path, pallete_name: str):
     """Render a new image applying Floyd-Steinberg dithering."""
     for y in range(image.height):
         for x in range(image.width):
             pixel = image.getpixel((x, y))
             color = pixel_to_color(pixel)
-            new_color = get_nearest_color(color, pallete=PALLETE_1)
+            new_color = get_nearest_color(color, pallete=PALLETE[pallete_name])
 
             draw = ImageDraw.Draw(image)
             draw.point((x, y), color_to_pixel(new_color))
@@ -140,15 +104,17 @@ def render(image: Image, output: str = 'imgout.png'):
                     color_to_pixel( add_colors(temp_color, multiply_color(quant_error, 1/16)) )
                 )
 
-    image.save(output)
+    new_image_name = f'{img_path.stem}-{pallete_name}{img_path.suffix}'
+    image.save(img_path.parent / new_image_name)
 
 
-def main():
-    img_path = 'images/boat.webp'
+@app.command()
+def run(path_to_image: str, pallete: str = 'B&W'):
+    img_path = Path(path_to_image)
     
     with Image.open(img_path) as image:
-        render(image, output='imgout2.png')
+        render(image, img_path, pallete)
 
 
 if __name__ == '__main__':
-    main()
+    app()
